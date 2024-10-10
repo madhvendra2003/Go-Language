b5e7c38d9d2ce6a6eb3fecfd402a5ac08a8c44bf
Madhvendra Singh <madvendra1789@gmail.com>
2024-10-10 19:37:39 +0530

    Restarrt

--- goLang/main.go
+++ goLang/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	myutil "golang/goLang/myUtil"
 )
 
 func main() {
 	fmt.Println("this side is madhvendra")
-	myutil.PrintMessage("this is from my util")
-	myutil.Add(2, 3)
 
 }
